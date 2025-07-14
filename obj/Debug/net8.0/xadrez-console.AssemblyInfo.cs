80f14a22d6874e9ddcb48ca85aafb54a33ac5ef1
Hillton Batista <hbatistahb7@gmail.com>
2025-07-14 16:50:43 -0300

    Tratando erros com excecao personalizada

--- obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68257f3047b0c439bbe7b8b0dca1e2bb4d9c944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bcd69261e13cb1c5d5e40a8b8a37c244812534")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
