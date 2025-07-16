a973f1c40a861773f3e2719bfe490c49f415c7a8
Hillton Batista <hbatistahb7@gmail.com>
2025-07-16 16:42:19 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-        Console.WriteLine(pos);
-        Console.WriteLine(pos.ToPosicao());
+        try
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            Tela.imprimirTabuleiro(tab);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
         Console.ReadLine();
     }
 }
--- obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f14a22d6874e9ddcb48ca85aafb54a33ac5ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f035ae9181825f05484fac501bee4079dd8274b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
